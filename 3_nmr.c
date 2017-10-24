39959ebdca5adb7c79c58b0306d8206b6da4de9c
Unknown <hfdbch@gmail.com>
2017-10-24 21:10:49 +0700

    kerangka main
    mulai dari awal

--- 3_nmr.c
+++ 3_nmr.c
@@ -1,9 +1,32 @@
-#include <stdio.h>
-
-int main()
+#include<stdio.h>
+#include<string.h>
+#include<pthread.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<stdlib.h>
+/*******************************************************
+*compile dengan cara gcc -pthread -o [output] input.c *
+*******************************************************/
+ 
+int main(void)
 {
-
-  // ini juga test
-
-  return 0;
+    int i=0;
+    int err;
+    while(i<2)//looping membuat thread 2x
+    {
+        err=pthread_create(&(tid[i]),NULL,&playandcount,NULL);//membuat thread
+        if(err!=0)//cek error
+        {
+            printf("\n can't create thread : [%s]",strerror(err));
+        }
+        else
+        {
+            printf("\n create thread success");
+        }
+        i++;
+    }
+    pthread_join(tid[0],NULL);
+    pthread_join(tid[1],NULL);
+    return 0;
 }
+
