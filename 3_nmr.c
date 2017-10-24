ce24cb622d6160556ccf7dd2911bbd66882efb5a
Unknown <hfdbch@gmail.com>
2017-10-24 21:46:25 +0700

    Add feed_lohan & feed_crab func.

--- 3_nmr.c
+++ 3_nmr.c
@@ -7,14 +7,21 @@
 /*******************************************************
 *compile dengan cara gcc -pthread -o [output] input.c *
 *******************************************************/
- 
+
+pthread_t tid[2];
+int status_crab = status_lohan = 100; // status awal
+void * feed_lohan();
+void * feed_crab();
+
 int main(void)
 {
     int i=0;
     int err;
+    pthread_t crab, lohan; // inisialisasi trit awal
+
     while(i<2)//looping membuat thread 2x
     {
-        err=pthread_create(&(tid[i]),NULL,&playandcount,NULL);//membuat thread
+        err=pthread_create(&(tid[i]),NULL,NULL,NULL);//membuat thread
         if(err!=0)//cek error
         {
             printf("\n can't create thread : [%s]",strerror(err));
@@ -30,3 +37,12 @@ int main(void)
     return 0;
 }
 
+void * feed_lohan()
+{
+    status_lohan += 10;
+}
+
+void * feed_crab()
+{
+    status_crab += 10;
+}
