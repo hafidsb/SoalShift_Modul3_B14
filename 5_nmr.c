1132ed9420718368d8a464c928a624f887a76c51
hafidsb <hfdbch@gmail.com>
2017-10-25 17:33:47 +0700

    Ni no 5 udah bisa tapi pasti terakhirnya muncul seg fault
    terus gabisa sekali jalan

--- 5_nmr.c
+++ 5_nmr.c
@@ -3,20 +3,20 @@
 #include<pthread.h>
 #include<stdlib.h>
 #include<unistd.h>
-#include<stdlib.h>
 /*******************************************************
 *compile dengan cara gcc -pthread -o [output] input.c *
 *******************************************************/
 
 void *count_word(void *arg)
 {
-    printf("cek\n");
+    //printf("cek\n");
     FILE *f;
     char word_search[25]; // ini kata yang dicari
-    char word_read[666]; // ini isi file stream yang akan dibaca
+    char word_read[100]; // ini isi file stream yang akan dibaca
     int temp = 0;
     f = fopen("Novel.txt", "r");
     strcpy(word_search, arg);
+    //printf("%s\n", word_search);
     while(fscanf(f, "%s", word_read) != EOF)
     {
         if(strstr(word_read, word_search) != NULL) temp++;
@@ -27,20 +27,17 @@ void *count_word(void *arg)
 }
 
 int main(int argc, char *argu[])
-{
-    int i=1;
-    
+{   
     pthread_t tid[argc]; // isinya trit-trit gitu sesuai banyak input
-    while(i<argc)//looping membuat thread sebanyak argumen
+    int i;
+    for(i=1; i<=argc; i++)//looping membuat thread sebanyak argumen
     {
         pthread_create(&(tid[i]),NULL, &count_word, (void *)argu[i]);//membuat thread
-        i++;
     }
 
-    while(i<argc)// pthread_join sebanyak jml argumen
+    for(i=1; i<=argc; i++)// pthread_join sebanyak jml argumen
     {
         pthread_join(tid[i],NULL);
-        i++;
     }
     
     return 0;
